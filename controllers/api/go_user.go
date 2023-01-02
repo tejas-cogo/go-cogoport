1a71ddb244446c78ffa0b3ebd1c5a6df3093036c
sanmit-vartak <sanmit.vartak@cogoport.com>
2023-01-02 13:40:16 +0530

    go users

--- controllers/api/go_user.go
+++ controllers/api/go_user.go
@@ -2,11 +2,9 @@ package controllers
 
 import (
 	"github.com/gin-gonic/gin"
-	hlp "github.com/tejas-cogo/go-cogoport/helpers"
 	service "github.com/tejas-cogo/go-cogoport/services/api/users"
 )
 
 func UserList(c *gin.Context) {
-	resp := service.UserList()
-	hlp.Respond(c.Writer, resp)
+	c.JSON(200, service.UserList())
 }
