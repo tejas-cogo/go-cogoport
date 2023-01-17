9b4e172b02d034053d7f23dc8ab7a68f7e939de9
Shikha <shikha.chandel@cogoport.com>
2023-01-17 12:36:03 +0530

    list group memebr

--- routes/routes.go
+++ routes/routes.go
@@ -20,6 +20,7 @@ func SetupRouter() *gin.Engine {
 	ticket_system.POST("delete_group", controllers.DeleteGroup)
 	ticket_system.POST("update_group", controllers.UpdateGroup)
 
+	ticket_system.GET("list_group_member", controllers.ListGroupMember)
 	ticket_system.POST("create_group_member", controllers.CreateGroupMember)
 	ticket_system.POST("delete_group_member", controllers.DeleteGroupMember)
 	ticket_system.POST("update_group_member", controllers.UpdateGroupMember)
