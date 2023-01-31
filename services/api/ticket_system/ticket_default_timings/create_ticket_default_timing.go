a8b57324a8c245e5cf0dc315dfacf63336142767
Shikha <shikha.chandel@cogoport.com>
2023-01-31 20:48:11 +0530

    changes

--- services/api/ticket_system/ticket_default_timings/create_ticket_default_timing.go
+++ services/api/ticket_system/ticket_default_timings/create_ticket_default_timing.go
@@ -1,16 +1,18 @@
 package ticket_system
 
 import (
+	"errors"
+
 	"github.com/tejas-cogo/go-cogoport/config"
 	"github.com/tejas-cogo/go-cogoport/models"
-	"errors"
+	helpers "github.com/tejas-cogo/go-cogoport/services/helpers"
 )
 
 type TicketDefaultTimingService struct {
 	TicketDefaultTiming models.TicketDefaultTiming
 }
 
-func CreateTicketDefaultTiming(ticket_default_timing models.TicketDefaultTiming) (models.TicketDefaultTiming,error) {
+func CreateTicketDefaultTiming(ticket_default_timing models.TicketDefaultTiming) (models.TicketDefaultTiming, error) {
 	db := config.GetDB()
 	tx := db.Begin()
 	var err error
@@ -48,6 +50,13 @@ func validate(ticket_default_timing models.TicketDefaultTiming) string {
 
 	if ticket_default_timing.TicketDefaultTypeID == 0 {
 		return ("Ticket Default Type Is Required!")
+	}
+
+	ExpiryDuration := helpers.GetDuration(ticket_default_timing.ExpiryDuration)
+	Tat := helpers.GetDuration(ticket_default_timing.Tat)
+
+	if ExpiryDuration <= Tat {
+		return ("Expiry Duration should be greater than Tat!")
 	}
 
 	return ("validated")
