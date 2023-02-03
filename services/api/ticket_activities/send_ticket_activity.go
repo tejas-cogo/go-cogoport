cf20836993304724f701171f94a4d2a4d366313a
Shikha <shikha.chandel@cogoport.com>
2023-02-03 18:36:29 +0530

    ticket default group levels

--- services/api/ticket_activities/send_ticket_activity.go
+++ services/api/ticket_activities/send_ticket_activity.go
@@ -1,70 +1,70 @@
 package api
 
 import (
-	"bytes"
-	"encoding/json"
-	"io/ioutil"
-	"log"
-	"net/http"
+	_ "bytes"
+	_ "encoding/json"
+	_ "io/ioutil"
+	_ "log"
+	_ "net/http"
 
-	"github.com/tejas-cogo/go-cogoport/config"
+	_ "github.com/tejas-cogo/go-cogoport/config"
 	"github.com/tejas-cogo/go-cogoport/models"
 )
 
 func SendTicketActivity(ticket_activity models.TicketActivity) {
-	db := config.GetDB()
+	// db := config.GetDB()
 
-	if ticket_activity.Type == "respond" {
-		var ticket_user models.TicketUser
+	// if ticket_activity.Type == "respond" {
+	// 	var ticket_user models.TicketUser
 
-		db.Where("id = ?", ticket_activity.TicketUserID).First(&ticket_user)
+	// 	db.Where("id = ?", ticket_activity.TicketUserID).First(&ticket_user)
 
-		hc := http.Client{}
+	// 	hc := http.Client{}
 
-		var body models.Post
+	// 	var body models.Post
 
-		// ticket_user.SystemUserID
+	// 	// ticket_user.SystemUserID
 
-		body.Recipient = ticket_user.Email
-		body.Type = "email"
-		body.Service = "user"
-		body.ServiceID = "f06b29c0-b443-4f71-bf64-b61106dcaaf8"
-		body.TemplateName = "Ticket System"
+	// 	body.Recipient = ticket_user.Email
+	// 	body.Type = "email"
+	// 	body.Service = "user"
+	// 	body.ServiceID = "f06b29c0-b443-4f71-bf64-b61106dcaaf8"
+	// 	body.TemplateName = "Ticket System"
 
-		reqBody, err := json.Marshal(body)
+	// 	reqBody, err := json.Marshal(body)
 
-		req, err := http.NewRequest("POST", "https://api-apollo3.dev.cogoport.io/communication/create_communication", bytes.NewBuffer(reqBody))
-		if err != nil {
-			log.Printf("Request Failed: %s", err)
-			return
-		}
+	// 	req, err := http.NewRequest("POST", "https://api-apollo3.dev.cogoport.io/communication/create_communication", bytes.NewBuffer(reqBody))
+	// 	if err != nil {
+	// 		log.Printf("Request Failed: %s", err)
+	// 		return
+	// 	}
 
-		req.Header.Add("Authorization", "Bearer: 787b8f21-ca0a-4e79-af6e-81e3ca847909")
-		req.Header.Add("AuthorizationScope", "partner")
+	// 	req.Header.Add("Authorization", "Bearer: 787b8f21-ca0a-4e79-af6e-81e3ca847909")
+	// 	req.Header.Add("AuthorizationScope", "partner")
 
-		req.Header.Add("Content-type", "application/json; charset=utf-8")
+	// 	req.Header.Add("Content-type", "application/json; charset=utf-8")
 
-		resp, err := hc.Do(req)
+	// 	resp, err := hc.Do(req)
 
-		if err == nil {
+	// 	if err == nil {
 
-			defer resp.Body.Close()
-		}
+	// 		defer resp.Body.Close()
+	// 	}
 
-		resp_body, err := ioutil.ReadAll(resp.Body)
+	// 	resp_body, err := ioutil.ReadAll(resp.Body)
 
-		// Log the request body
-		bodyString := string(resp_body)
-		log.Print(bodyString)
-		// Unmarshal result
-		post := models.Post{}
-		err = json.Unmarshal(resp_body, &post)
-		if err != nil {
-			log.Printf("Reading body failed: %s", err)
-			return
-		}
+	// 	// Log the request body
+	// 	bodyString := string(resp_body)
+	// 	log.Print(bodyString)
+	// 	// Unmarshal result
+	// 	post := models.Post{}
+	// 	err = json.Unmarshal(resp_body, &post)
+	// 	if err != nil {
+	// 		log.Printf("Reading body failed: %s", err)
+	// 		return
+	// 	}
 
-		log.Printf("Successfully Created")
+	// 	log.Printf("Successfully Created")
 
-	}
+	// }
 }
