45263606bcc6b00775ca717826f0ad6cdd16a17a
sanmit-vartak <sanmit.vartak@cogoport.com>
2023-02-06 21:47:22 +0530

    fix

--- services/api/tickets/list_tickets.go
+++ services/api/tickets/list_tickets.go
@@ -19,7 +19,7 @@ func ListTicket(filters models.TicketExtraFilter) ([]models.Ticket, *gorm.DB) {
 	var ticket []models.Ticket
 
 	if filters.MyTicket != "" {
-		db = db.Where("user_id = ?", filters.MyTicket).Distinct("id").Order("id").Find(&ticket).Pluck("id", &ticket_id)
+		db.Where("user_id = ?", filters.MyTicket).Distinct("id").Order("id").Find(&ticket).Pluck("id", &ticket_id)
 
 	} else {
 		if filters.AgentRmID != "" {
