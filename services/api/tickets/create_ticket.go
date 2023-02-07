31bf8b120dc69a97453ba60c59d02162d6f1cfe8
sanmit-vartak <sanmit.vartak@cogoport.com>
2023-02-07 14:53:59 +0530

    create ticket user

--- services/api/tickets/create_ticket.go
+++ services/api/tickets/create_ticket.go
@@ -8,6 +8,7 @@ import (
 	"github.com/tejas-cogo/go-cogoport/models"
 	audits "github.com/tejas-cogo/go-cogoport/services/api/ticket_audits"
 	reviewers "github.com/tejas-cogo/go-cogoport/services/api/ticket_reviewers"
+	ticket_users "github.com/tejas-cogo/go-cogoport/services/api/ticket_users"
 	helpers "github.com/tejas-cogo/go-cogoport/services/helpers"
 	validations "github.com/tejas-cogo/go-cogoport/services/validations"
 )
@@ -18,7 +19,6 @@ type TicketService struct {
 
 func CreateTicket(ticket models.Ticket) (models.Ticket, error) {
 	db := config.GetDB()
-	db2 := config.GetCDB()
 
 	tx := db.Begin()
 	var err error
@@ -27,8 +27,6 @@ func CreateTicket(ticket models.Ticket) (models.Ticket, error) {
 	var user models.User
 	var ticket_default_type models.TicketDefaultType
 	var ticket_default_timing models.TicketDefaultTiming
-	db2.Where("id = ?", ticket.UserID).First(&user)
-	// TODO: CreateTicketUser
 
 	if ticket.TicketUserID != 0 {
 		if err := tx.Where("id = ? ", ticket.TicketUserID).First(&ticket_user).Error; err != nil {
@@ -36,9 +34,19 @@ func CreateTicket(ticket models.Ticket) (models.Ticket, error) {
 			return ticket, errors.New(err.Error())
 		}
 		if ticket_user.ID == 0 {
-			return ticket, errors.New("System User Not Found")
+			return ticket, errors.New("system user not found")
 		}
 		ticket.UserID = ticket_user.SystemUserID
+	} else {
+		db2 := config.GetCDB().Debug()
+		var new_ticket_user models.TicketUser
+		db2.Where("id = ?", ticket.UserID).First(&user)
+		new_ticket_user.Email = user.Email
+		new_ticket_user.SystemUserID = user.ID
+		new_ticket_user.Name = user.Name
+		new_ticket_user.MobileNumber = user.MobileNumber
+		ticket_user, _ := ticket_users.CreateTicketUser(new_ticket_user)
+		ticket.TicketUserID = ticket_user.ID
 	}
 
 	if err := tx.Where("ticket_type = ? and status = ? ", ticket.Type, "active").First(&ticket_default_type).Error; err != nil {
