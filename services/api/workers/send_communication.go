5e018bebdeecef3967b64652b47ad0d47a1e8a58
rhythmbagde <Rhythm.Bagde@cogoport.com>
2023-02-08 18:05:08 +0530

    done

--- services/api/workers/send_communication.go
+++ services/api/workers/send_communication.go
@@ -11,14 +11,6 @@ import (
 )
 
 func SendCommunications(ID uint) {
-	db := config.GetDB()
-
-	var ticket models.Ticket
-	db.Where("id = ?", ID).First(&ticket)
-
-	var ticket_user models.TicketUser
-	db.Where("id = ?", ticket.TicketUserID).First(&ticket_user)
-
 	type Response struct {
 		ID uuid.UUID
 	}
@@ -27,23 +19,41 @@ func SendCommunications(ID uint) {
 	var body models.Communication
 	var communication_id Response
 
+	db := config.GetDB()
+
+	var ticket models.Ticket
+	db.Where("id = ?", ID).First(&ticket)
+
+	var ticket_user models.TicketUser
+	db.Where("id = ?", ticket.TicketUserID).First(&ticket_user)
+	
 	rubyclient.Endpoint = "communication/create_communication"
 
-	body.Recipient = ticket_user.Email
-	body.Type = "email"
-	body.Service = "user"
-	body.ServiceID = "f06b29c0-b443-4f71-bf64-b61106dcaaf8"
-	body.TemplateName = "Ticket System"
-
-	obj, _ := helpers.PostRubyClient(body, rubyclient)
-
-	bodyString := string(obj)
-	err := json.Unmarshal([]byte(bodyString), &communication_id)
-	if err != nil {
-		log.Println(err)
-	}
-
-	if communication_id.ID != uuid.Nil {
-		log.Printf("Successfully Created!")
+	for _, preference := range ticket.NotificationPreferences {
+		if preference == "email"{
+			body.Recipient = ticket_user.Email
+			body.Type = "email"
+			body.Service = "user"
+			body.ServiceID = "f06b29c0-b443-4f71-bf64-b61106dcaaf8"
+			body.TemplateName = "Ticket System"
+		} else if preference == "whatsapp"{
+			body.Recipient = ticket_user.MobileNumber
+			body.Type = "whatsapp"
+			body.Service = "user"
+			body.ServiceID = "f06b29c0-b443-4f71-bf64-b61106dcaaf8"
+			body.TemplateName = "Ticket System"
+		}
+	
+		obj, _ := helpers.PostRubyClient(body, rubyclient)
+	
+		bodyString := string(obj)
+		err := json.Unmarshal([]byte(bodyString), &communication_id)
+		if err != nil {
+			log.Println(err)
+		}
+	
+		if communication_id.ID != uuid.Nil {
+			log.Printf("Successfully Created!")
+		}
 	}
 }
